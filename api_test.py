b38f76732a1d44199f246fdf7e8fd20f57051e42
David <david.niemann@hs-furtwangen.de>
2022-05-21 14:57:02 +0200

    restore covers

--- api_test.py
+++ api_test.py
@@ -23,12 +23,10 @@ BASE_URL = "https://api.spotify.com/v1/"
 
 COVER_SIZE = 10  # size of the total cover
 # distance between the color components until a new material is created
-COLOR_DIFFERECE = 0.2
-COVER_POSITION = (6, 0, 38)
+COVER_POSITION = (0, 0, 38)
 
-plane_amount = 200  # Length of the square covers
-materials = []  # saves all materials
-material_index = []  # Indices which materials should be assigned to the faces
+PIXEL_LEVEL = 0.01
+
 
 # get access token
 auth_response = requests.post(AUTH_URL, {
@@ -199,31 +197,50 @@ def createCoverFromImage(img):
     global COVER_SIZE
     global COVER_POSITION
     bpy.ops.mesh.primitive_plane_add(
-        size=COVER_SIZE, location=COVER_POSITION, rotation=(pi/2, pi, pi/2))
+        size=COVER_SIZE, location=COVER_POSITION, rotation=(pi/2, 0, pi))
     cover_object: bpy.types.Object = bpy.data.objects["Plane"]
     mat = createCoverMaterial(img)
     cover_object.data.materials.append(mat)
- 
+    cover_object.name = "cover"
 
 
 def createCoverMaterial(cover_img):
     global COVER_SIZE
+    global PIXEL_LEVEL
     mat: bpy.types.Material = bpy.data.materials.new("mat_Cover")
     mat.use_nodes = True
-    nodeTree: bpy.types.NodeTree = mat.node_tree
+    node_tree: bpy.types.NodeTree = mat.node_tree
+
     bsdf = mat.node_tree.nodes["Principled BSDF"]
-    texImage = nodeTree.nodes.new('ShaderNodeTexImage')
+    tex_image = node_tree.nodes.new('ShaderNodeTexImage')
+
     rgba = cv2.cvtColor(cover_img, cv2.COLOR_RGB2BGRA)
-    rows, cols, colors = cover_img.shape
-    l = rgba.reshape(-2)
-    listpixel = l.tolist()
-    pxl = [i/255 for i in listpixel]
+    rows, cols, _ = cover_img.shape
+    reversed_y = rgba[::-1]
+    l = reversed_y.reshape(-2)
+    list_pixel = l.tolist()
+    pxl = [i/255 for i in list_pixel]
     img = bpy.data.images.new(
         "cover image", width=rows, height=cols)
     img.pixels.foreach_set(pxl)
-    texImage.image = img
-    nodeTree.links.new(
-        bsdf.inputs['Base Color'], texImage.outputs['Color'])
+    tex_image.image = img
+
+    node_tree.links.new(
+        bsdf.inputs['Base Color'], tex_image.outputs['Color'])
+    vcector_math = node_tree.nodes.new('ShaderNodeVectorMath')
+    node_tree.links.new(
+        tex_image.inputs['Vector'], vcector_math.outputs['Vector'])
+    vcector_math.operation = 'SNAP'
+
+    vcector_math.inputs[1].default_value[0] = PIXEL_LEVEL
+    vcector_math.inputs[1].default_value[1] = PIXEL_LEVEL
+    vcector_math.inputs[1].default_value[2] = PIXEL_LEVEL
+
+    tex_coordinates = node_tree.nodes.new('ShaderNodeTexCoord')
+    node_tree.links.new(
+        vcector_math.inputs['Vector'], tex_coordinates.outputs['Generated'])
+    node_tree.links.new(
+        bsdf.inputs['Emission'], tex_image.outputs['Color'])
     return mat
 
 # Clears console
