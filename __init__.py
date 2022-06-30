259b1ce4ed46d084aa0b879fd28eeb0c84eb5427
Daniel <daniel_meisler@web.de>
2022-06-30 20:01:41 +0200

    feat: comments

--- __init__.py
+++ __init__.py
@@ -11,6 +11,7 @@ import webbrowser
 import bpy
 import base64
 
+# information
 bl_info = {
     "name": "Spotify API Visualizer",
     "author": "Samuel Kasper, Alexander Reiprich, David Niemann, Daniel Meisler",
@@ -19,12 +20,14 @@ bl_info = {
     "category": "Add Mesh",
 }
 
-
+# main
 def main():
-    bpy.ops.object.select_all(action='SELECT')  # selektiert alle Objekte
-    # löscht selektierte objekte
+    # selects all objects
+    bpy.ops.object.select_all(action='SELECT')  
+    # deletes all object
     bpy.ops.object.delete(use_global=False, confirm=False)
-    bpy.ops.outliner.orphans_purge()  # löscht überbleibende Meshdaten etc.
+    # deletes all leftover meshdata etc.
+    bpy.ops.outliner.orphans_purge()  
 
 
 
@@ -43,7 +46,8 @@ AUTH_URL = "https://accounts.spotify.com/api/token"
 CLIENT_AUTH_URL = "https://accounts.spotify.com/authorize"
 BASE_URL = "https://api.spotify.com/v1/"
 
-COVER_SIZE = 1.2  # size of the total cover
+# size of the total cover
+COVER_SIZE = 1.2  
 # distance between the color components until a new material is created
 COVER_POSITION = (-2.6819, 1.10, 3.34549)
 PROFILE_POSITION = (0.229749, 1.1, 2.18236)
@@ -69,8 +73,10 @@ headers = {
     'Authorization': 'Bearer {token}'.format(token=access_token)
 }
 
+# dict with needed weblinks
 links = {"Spotify" : 'https://developer.spotify.com/console/get-users-currently-playing-track/?market=&additional_types=',}
 
+# propertygroup with needed properties for the panel
 class MyProperties(bpy.types.PropertyGroup):
     bl_idname = "MyProperties"
         
@@ -84,7 +90,7 @@ class MyProperties(bpy.types.PropertyGroup):
 
     refresh_timer: bpy.props.FloatProperty(name= "Timer", soft_min= 1, soft_max= 10, default= 1)
     
-
+# panel for the user input
 class SPOTIFY_PT_panel(bpy.types.Panel):
     """Creates a Panel in the scene context of the properties editor"""
     bl_label = "Spotify Addon"
@@ -106,7 +112,7 @@ class SPOTIFY_PT_panel(bpy.types.Panel):
         layout.prop(mytool, "refresh_timer")
         self.layout.operator('button.execute', text='Ausführen')
 
-
+# operator for the button on the panel
 class executeAction(bpy.types.Operator):
     bl_idname = "button.execute"
     bl_label = "execute"
@@ -118,6 +124,7 @@ class executeAction(bpy.types.Operator):
         Songcover()
         return {'FINISHED'}
 
+# start of the application
 class Songcover():
     def __init__(self):
         # clear()
@@ -151,9 +158,8 @@ class Songcover():
         webbrowser.open(url, new=0, autoraise=True)
 
 
-    # Gets the access token from the user code from the function above
-    # This code automatically gets stored in "access_token_user" and is needed for anything that has to do with user activity
-
+    # gets the access token from the user code from the function above
+    # this code automatically gets stored in "access_token_user" and is needed for anything that has to do with user activity
     def getAccessToken():
         encoded = base64.b64encode(
             (CLIENT_ID + ":" + CLIENT_SECRET).encode("utf-8")).decode("utf-8")
@@ -174,8 +180,7 @@ class Songcover():
         access_token_user = r.json().get("access_token")
         print(access_token_user)
 
-    # Gets playback state of current device, if true, something is playing
-
+    # gets playback state of current device, if true, something is playing
     def getCurPlaybackState():
         curPlayingUrl = BASE_URL + "me/player"
         header = {
@@ -186,8 +191,7 @@ class Songcover():
 
         return respJson["is_playing"]
 
-    # Returns the milliseconds that have passed since the song began
-
+    # returns the milliseconds that have passed since the song began
     def getMsIntoCurSong(): 
         curPlayingUrl = BASE_URL + "me/player"
         header = {
@@ -198,8 +202,7 @@ class Songcover():
         return respJson["progress_ms"]
 
 
-    # Returns the percentage of how much the song is over
-
+    # returns the percentage of how much the song is over
     def getProgressIntoCurSong(): 
 
         max_length = Songcover.getCurrentlyPlayedSong()["duration"]
@@ -207,8 +210,7 @@ class Songcover():
         return((cur_position / max_length) * 100)
 
 
-    # Returns an object containing information about the currently played song
-
+    # returns an object containing information about the currently played song
     def getCurrentlyPlayedSong():
         global access_token_user
         curPlayingUrl = BASE_URL + "me/player/currently-playing"
@@ -234,15 +236,13 @@ class Songcover():
         }
         return currentTrackInfo
 
-    # Gets artist and name of current song in this format:
+    # gets artist and name of current song in this format:
     # "artist, artist2 - song"
-
     def getArtistAndNameOfCurSong(): 
         currentTrackInfo = Songcover.getCurrentlyPlayedSong()
         return currentTrackInfo["artists"] + " - " + currentTrackInfo["name"]
 
-    # Gets the artist image of the current song
-
+    # gets the artist image of the current song
     def getArtistImage(track_id):
         track_req = requests.get(BASE_URL + "tracks/" + track_id, headers=headers)
         track_data = track_req.json()
@@ -255,8 +255,7 @@ class Songcover():
         img = cv2.imdecode(img_string, cv2.IMREAD_UNCHANGED)
         return img
 
-    # Gets the users profile image
-
+    # gets the users profile image
     def getLinkToCurUserImage():
         userUrl = BASE_URL + "me"
         header = {
@@ -275,8 +274,7 @@ class Songcover():
             img = cv2.imdecode(img_string, cv2.IMREAD_UNCHANGED)
             return img
 
-    # Gets current users top 3 artists of this month
-
+    # gets current users top 3 artists of this month
     def getCurUserTopArtists():
         topArtistsUrl = BASE_URL + "me/top/artists?time_range=medium_term&limit=3&offset=0"
         header = {
@@ -289,8 +287,7 @@ class Songcover():
 
         return topArtists
 
-    # Gets current users top track of this week
-
+    # gets current users top track of this week
     def getCurUserTopSong(): 
         topTrackUrl = BASE_URL + "me/top/tracks?time_range=short_term&limit=1&offset=0"
         header = {
@@ -301,8 +298,7 @@ class Songcover():
 
         return ["Currently Trending", respJson["items"][0]["name"]]
 
-    # Get current users display name
-
+    # get current users display name
     def getCurUserDisplayName():
         displayUrl = BASE_URL + "me"
         header = {
@@ -313,29 +309,26 @@ class Songcover():
         
         return respJson["display_name"]
 
-    # Gets cover of current song
-
+    # gets cover of current song
     def getCoverOfCurrentSong():
         currentTrackId = Songcover.getCurrentlyPlayedSong()["id"]
         Songcover.getSongImage(currentTrackId)
 
 
-    # Gets cover from track id
-
+    # gets cover from track id
     def getSongImage(track_id):
         global COVER_POSITION
         r = requests.get(BASE_URL + "tracks/" + track_id, headers=headers)
         d = r.json()
 
-        # Get image part
+        # get image part
         cover_image = requests.get(d["album"]["images"][0]['url'])
         img_string = np.frombuffer(cover_image.content, np.uint8)
         img = cv2.imdecode(img_string, cv2.IMREAD_UNCHANGED)
         Songcover.create_board_from_image(img, "cover", COVER_POSITION)
 
 
-    # Loop that checks if the song has changed
-
+    # loop that checks if the song has changed
     def updateCurrentSong():
         global song_id 
     
@@ -348,6 +341,7 @@ class Songcover():
             return True
         return False
             
+    # updates the cover if a new song is playing
     def update_cover(): 
         Songcover.delete_current_cover()
         Songcover.getCoverOfCurrentSong()
@@ -355,14 +349,15 @@ class Songcover():
         songdata = Songcover.getCurrentlyPlayedSong()
         titel.data.body = songdata["name"]
 
+    # deletes the cover if it is not playing anymore
     def delete_current_cover():
         bpy.ops.object.select_all(action='DESELECT')
         cover = bpy.context.scene.objects.get('cover')
         if cover:
             bpy.data.objects['cover'].select_set(True)
             bpy.ops.object.delete()
-    # Creates cover from image retrieved in getSongImage()
 
+    # creates cover from image retrieved in getSongImage()
     def create_board_from_image(img, name, position):
         global COVER_SIZE
 
@@ -375,6 +370,7 @@ class Songcover():
         cover_object.data.materials.append(mat)
         cover_object.name = name
 
+    # creates material for the cover
     def create_board_material(cover_img):
         global COVER_SIZE
         global PIXEL_LEVEL
@@ -415,6 +411,7 @@ class Songcover():
             bsdf.inputs['Emission'], tex_image.outputs['Color'])
         return mat
 
+    # creates the song title on the tram
     def create_song_titel():
         mat = bpy.data.materials.get("Window_Light")
         songdata = Songcover.getCurrentlyPlayedSong()
@@ -432,6 +429,7 @@ class Songcover():
         titel_obj.location = (+4, +0.35, -0.78)
         titel_obj.color = (0, 0, 0, 0)
 
+    # creates the spotify user name on the left panel
     def create_display_name():
         mat = bpy.data.materials.get("Window_Light")
         displayname = Songcover.getCurUserDisplayName()
@@ -450,6 +448,7 @@ class Songcover():
         titel_obj.location = (-0.34, -0.85, -0.9)
         titel_obj.color = (0, 0, 0, 0)
 
+    # creates the top artists on the right panel
     def create_top_artists():
         mat = bpy.data.materials.get("Window_Light")
         artists = Songcover.getCurUserTopArtists()
@@ -468,6 +467,7 @@ class Songcover():
         artists_obj.location = (1.32, -0.88, -0.2)
         artists_obj.color = (0, 0, 0, 0)
 
+    # creates the top tracks on the blimp panel
     def create_top_track(): 
         mat = bpy.data.materials.get("Window_Light")
         track = Songcover.getCurUserTopSong()[1]
@@ -485,7 +485,7 @@ class Songcover():
         track_obj.location = (0.38, -0.75, 0.94)
         track_obj.color = (0, 0, 0, 0)
 
-    # Clears console
+    # clears the environment to start on an empty space
     def clear_environment(): 
         # Select all objects
         bpy.ops.object.select_all(action='SELECT')
@@ -497,9 +497,11 @@ class Songcover():
         for material in bpy.data.materials:
             bpy.data.materials.remove(material, do_unlink=True)
 
+    # clears console
     def clear_console():
         os.system('cls')
     
+    # takes the user input on how fast should be everything refreshed
     def run_every_n_second():
         global WAIT_TIME
         WAIT_TIME = bpy.data.scenes["Scene"].my_tool.refresh_timer
@@ -516,6 +518,7 @@ class Songcover():
         #counter += 1
         return WAIT_TIME   
 
+    # refreshes the blimp panel with the top artists from the current user
     def update_top_artist():
         global CURRENT_ARTIST_POS
         allArtists = Songcover.getCurUserTopArtists()
@@ -529,6 +532,7 @@ class Songcover():
 
         return ARTIST_CHANGE_TIME
 
+    # refreshes the left panel with the top track that is currently trending
     def update_trending_track():
         global TRENDING_CHANGE_TIME
         track = bpy.data.objects["Top-Track"]
@@ -542,8 +546,6 @@ class Songcover():
 
 
     # import assets for the environment
-
-
     def create_environment():
         file_path = 'DAVT_Project_Scene.blend'
         inner_path = 'Object'
@@ -563,6 +565,7 @@ class Songcover():
         Songcover.create_top_artists()
         Songcover.create_top_track()
 
+    # sets all animations
     def animation_handler():
 
         # delete old keyframes / animation
@@ -579,6 +582,7 @@ class Songcover():
         Songcover.world_background_animation(last_frame)
         Songcover.train_animation(last_frame)
 
+    # sun animation
     def sun_animation(last_frame):
         # variables
         sun_start_x = 27
@@ -613,6 +617,7 @@ class Songcover():
         sun.keyframe_insert(data_path="rotation_euler", frame=last_frame)
         sun_spot.keyframe_insert(data_path="color", frame=last_frame)
 
+    # heaven animation
     def world_background_animation(last_frame):
         world_bg_dark = (0.013,0.016,0.024,1)
         world_bg_bright = (0.085, 0.097,0.138, 1)
@@ -626,6 +631,7 @@ class Songcover():
         world_background.default_value = world_bg_bright
         world_background.keyframe_insert(data_path="default_value", frame=math.floor(last_frame/2))
 
+    # train animation
     def train_animation(last_frame):
         train_start_x = 8
         train_end_x = -10
@@ -645,10 +651,11 @@ class Songcover():
         train.location[0] = train_end_x
         train.keyframe_insert(data_path="location", frame=start_frame+train_duration)
 
+    # the sun will be set to the current frame
     def set_sun_to_curr_frame():
         bpy.data.scenes["Scene"].frame_current = math.floor((Songcover.getMsIntoCurSong()/1000)*ANIM_FRAME_RATE)
 
-
+# autostart
 class Autostart(bpy.types.Operator):
     bl_idname = "spotify.api"
     bl_label = "Spotify API Visualizer"
@@ -662,7 +669,7 @@ class Autostart(bpy.types.Operator):
         main()
         return {'FINISHED'}
 
-
+# all registered classes and properties
 def register():
     bpy.utils.register_class(Autostart)
     bpy.utils.register_class(MyProperties)
@@ -670,6 +677,7 @@ def register():
     bpy.utils.register_class(executeAction)
     bpy.types.Scene.my_tool = bpy.props.PointerProperty(type= MyProperties)
 
+# all unregistered classes and properties
 def unregister():
     bpy.utils.unregister_class(Autostart)
     bpy.utils.unregister_class(MyProperties)
@@ -677,6 +685,6 @@ def unregister():
     bpy.utils.unregister_class(executeAction)
     del bpy.types.Scene.my_tool
 
-
+# name
 if __name__ == "__main__":
     register()
